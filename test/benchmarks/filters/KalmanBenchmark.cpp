d96b6ab75e16342a777ca0d94af0a7b757e25655
rolfvdhulst <rolfvdhulst@gmail.com>
2020-04-22 14:19:47 +0200

    static circular buffer and new filters

--- test/benchmarks/filters/KalmanBenchmark.cpp
+++ test/benchmarks/filters/KalmanBenchmark.cpp
@@ -3,6 +3,8 @@
 //
 #include <benchmark/benchmark.h>
 #include <math/filters/KalmanFilter.h>
+#include <math/filters/PosVelFilter2D.h>
+#include <core/Random.h>
 
 //easily adjust the dimensions of the KF's tested here
 #define S 4
@@ -72,8 +74,71 @@ static void totalTime(benchmark::State& state){
     benchmark::DoNotOptimize(kState);
   }
 }
+
+std::vector<std::pair<Time,Eigen::Vector2d>> samples(){
+  int numSamples = 101;
+  double velocity = 3.0;
+  Random gen(57);
+  std::vector<std::pair<Time,Eigen::Vector2d>> measurements;
+  for (int i = 0; i < numSamples; i++) {
+    Time time(0.01*i);
+    double posX = time.asSeconds()*velocity + gen.getGaussian()*0.02; //2 cm noise
+    double posY = time.asSeconds()*velocity +gen.getGaussian()*0.02; //
+    measurements.push_back(std::make_pair(time,Eigen::Vector2d(posX,posY)));
+  }
+  return measurements;
+}
+static void predictTimePosVelFilter(benchmark::State& state) {
+  Eigen::Vector4d initialState = Eigen::Vector4d::Zero();
+
+  auto measurements = samples();
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    PosVelFilter2D filter(initialState, Eigen::Matrix4d::Identity(), 0.1, 0.01, Time());
+    for (const auto& z : measurements) {
+      filter.predict(z.first);
+    }
+    Eigen::Vector2d kState = filter.getPosition();
+    // Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(kState);
+  }
+}
+static void updateTimePosVelFilter(benchmark::State& state) {
+  Eigen::Vector4d initialState = Eigen::Vector4d::Zero();
+
+  auto measurements = samples();
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    PosVelFilter2D filter(initialState, Eigen::Matrix4d::Identity(), 0.1, 0.01, Time());
+    for (const auto& z : measurements) {
+      filter.update(z.second);
+    }
+    Eigen::Vector2d kState = filter.getPosition();
+    // Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(kState);
+  }
+}
+static void totalTimePosVelFilter(benchmark::State& state) {
+  Eigen::Vector4d initialState = Eigen::Vector4d::Zero();
+
+  auto measurements = samples();
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    PosVelFilter2D filter(initialState, Eigen::Matrix4d::Identity(), 0.1, 0.01, Time());
+    for (const auto& z : measurements) {
+      filter.predict(z.first);
+      filter.update(z.second);
+    }
+    Eigen::Vector2d kState = filter.getPosition();
+    // Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(kState);
+  }
+}
 // Register the function as a benchmark
-BENCHMARK(predictTime);
-BENCHMARK(updateTime);
+//BENCHMARK(predictTime);
+//BENCHMARK(updateTime);
 BENCHMARK(totalTime);
 
+//BENCHMARK(predictTimePosVelFilter);
+//BENCHMARK(updateTimePosVelFilter);
+BENCHMARK(totalTimePosVelFilter);
---
+++ test/common/containers/CircularBufferTest.cpp
@@ -0,0 +1,73 @@
+//
+// Created by rolf on 21-04-20.
+//
+
+#include <containers/circular_buffer.h>
+#include <array>
+#include <gtest/gtest.h>
+#include <numeric>
+void access(){
+  std::array<double,1> s = {9.0};
+  double x = s[10];
+  exit(0);
+}
+TEST(CircularBufferTest,aTest){
+  ASSERT_EXIT(access(),::testing::KilledBySignal(SIGSEGV),".*");
+}
+
+TEST(CircularBufferTest,construct){
+  circular_buffer<int,4> test;
+  for (int kI = 0; kI < 5; ++kI) {
+    test.push_back(kI*kI);
+  }
+  EXPECT_EQ(test.size(),4);
+  EXPECT_EQ(test.back(),16);
+  EXPECT_EQ(test.front(),1);
+
+  EXPECT_EQ(test.pop_front(),1);
+  EXPECT_EQ(test.size(),3);
+  EXPECT_EQ(test.max_size(),4);
+  EXPECT_EQ(test.front(),4);
+  EXPECT_EQ(test.back(),16);
+}
+void addVal(int &val){
+  val+=4;
+}
+TEST(CircularBufferTest,iteratorBasics){
+  circular_buffer<int,4> test;
+  for (int kI = 0; kI < 5; ++kI) {
+    test.push_back(kI*kI);
+  }
+  int index = 1;
+  for (const auto& elem : test) {
+    EXPECT_EQ(elem,index*index);
+    index++;
+  }
+  std::for_each(test.begin(),test.end(),addVal);
+  index = 1;
+  for (const auto& elem : test) {
+    EXPECT_EQ(elem,index*index+4);
+    index++;
+  }
+  int sum = std::accumulate(test.begin(),test.end(),0);
+  int check = 4*4 + 1 + 4 + 9 + 16;
+  EXPECT_EQ(sum,check);
+
+  sum = std::accumulate(test.rbegin(),test.rend(),0);
+  check = 4*4 + 1 + 4 + 9 + 16;
+  EXPECT_EQ(sum,check);
+
+  sum = std::accumulate(test.cbegin(),test.cend(),0);
+  check = 4*4 + 1 + 4 + 9 + 16;
+  EXPECT_EQ(sum,check);
+
+  sum = std::accumulate(test.crbegin(),test.crend(),0);
+  check = 4*4 + 1 + 4 + 9 + 16;
+  EXPECT_EQ(sum,check);
+
+  index = 4;
+  for (circular_buffer<int,4>::reverse_iterator it = test.rbegin(); it != test.rend(); it++) {
+    EXPECT_EQ(*it,index*index+4);
+    index--;
+  }
+}
