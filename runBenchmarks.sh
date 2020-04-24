724ea152bdfd789ab9d9afe6eadc229d75f81e2a
rolfvdhulst <rolfvdhulst@gmail.com>
2020-04-25 01:22:18 +0200

    update benchmarks script

--- runBenchmarks.sh
+++ runBenchmarks.sh
@@ -1,5 +1,6 @@
+rm -rf build
 mkdir build
 cd build
 cmake -DBENCHMARK=ON ..
 make math geometryBenchmarks -j6
-./test/benchmarks/geometryBenchmarks --benchmark_repetitions=5 --benchmark_report_aggregates_only=true
+./test/benchmarks/geometryBenchmarks --benchmark_repetitions=10 --benchmark_report_aggregates_only=true
