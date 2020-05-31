08815e5bc3fc427dc628ed7e28af5196550cd1a3
rolf <rolfvdhulst@gmail.com>
2020-05-31 21:10:02 +0200

    More structure and 1d constant velocity kalman filter

--- src/framework/visionFilter/CameraFilter.cpp
+++ src/framework/visionFilter/CameraFilter.cpp
@@ -4,3 +4,68 @@
 
 #include "CameraFilter.h"
 
+void CameraFilter::process(const DetectionFrame &frame) {
+    if(lastDetectionFrameTime<frame.timeCaptured){
+        lastDetectionFrameTime = frame.timeCaptured;
+    }else{
+        std::cerr<<"Delayed? Frame with " <<(lastDetectionFrameTime-frame.timeCaptured).asMilliSeconds()<<" ms not accepted!"<<std::endl;
+        return;
+    }
+    processRobots(frame,true);
+    processRobots(frame,false);
+    processBalls(frame);
+}
+
+void CameraFilter::processRobots(const DetectionFrame &frame, bool blueBots) {
+    robotMap& robots = blueBots ? blue : yellow;
+
+    const std::vector<RobotObservation>& detectionRobots = blueBots ? frame.blue: frame.yellow;
+    //Predict until the frame capture time
+    for(auto& oneIDFilters : robots){
+        for(auto& filter: oneIDFilters.second){
+            filter.predict(frame.timeCaptured);
+        }
+    }
+    //Pass observations to the filters.
+    //Note multiple observations of the same object can be made in this frame if they are close enough to the prediction
+    for(const RobotObservation& robot : detectionRobots){
+        std::vector<RobotFilter>& oneIDFilters = robots[robot.bot.robot_id()];
+        bool accepted = false;
+        //Attempt to update all filters that track robots with that id.
+        for(RobotFilter& filter : oneIDFilters){
+            accepted |= filter.update(robot);
+        }
+        //No existing filters accepted the robot detection so we create a new one with this detection
+        if(!accepted){
+            oneIDFilters.emplace_back(RobotFilter(robot));
+        }
+    }
+}
+
+void CameraFilter::processBalls(const DetectionFrame &frame) {
+    //Predict all existing filters to the frame capture time
+    for(auto& ballFilter : balls){
+        ballFilter.predict(frame.timeCaptured);
+    }
+    //Pass observations to the filters and update their error estimates
+    //Note multiple observations can be passed. It sometimes happens that the ball is detected as 2 separate
+    //balls so this is useful/necessary
+    for(const auto & ball : frame.balls){
+        bool accepted = false;
+        for(BallFilter& ballFilter : balls){
+            accepted |= ballFilter.update(ball);
+        }
+        //If no filters accepted the detected ball we create a new one with this detection
+        if(!accepted){
+            balls.emplace_back(BallFilter(ball));
+        }
+    }
+}
+
+void CameraFilter::updateCalibration(const Camera &calibration) {
+    camera=calibration;
+}
+
+void CameraFilter::updateRobotInfo(const proto::TeamRobotInfo& robotInfo) {
+    teamInfo = robotInfo;
+}
--- src/framework/visionFilter/include/visionFilter/CameraFilter.h
+++ src/framework/visionFilter/include/visionFilter/CameraFilter.h
@@ -11,15 +11,27 @@
 #include <protobuf/World.pb.h>
 #include <field/Camera.h>
 #include <containers/circular_buffer.h>
+#include <vision/DetectionFrame.h>
+#include <protobuf/RobotInfo.h>
+
 class CameraFilter {
 private:
     typedef std::map<int, std::vector<RobotFilter>> robotMap;
     robotMap blue;
     robotMap yellow;
     std::vector<BallFilter> balls; // A list containing all Filters that are tracking balls
-    std::optional<Camera> camera; //Position and direction data of the camera
     circular_buffer<BallObservation,100> lastDetections;
+
+    //the following we might want to pass as parameters instead of saving htem here
+    std::optional<Camera> camera; //Position and direction data of the camera
+    proto::TeamRobotInfo teamInfo;
+    Time lastDetectionFrameTime;
 public:
+    void updateCalibration(const Camera &camera);
+    void updateRobotInfo(const proto::TeamRobotInfo& robotInfo);
+    void process(const DetectionFrame& frame);
+    void processRobots(const DetectionFrame &frame, bool blueBots);
+    void processBalls(const DetectionFrame &frame);
 };
 
 #endif  // RTT_CAMERAFILTER_H
